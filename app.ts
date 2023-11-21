c8b0fb20f657c847594d552dbc7e4d0ea08c62fa
Ashik-Rahman100 <ashik5515@gmail.com>
2023-11-21 10:18:22 +0600

    initial server setup done

---
+++ app.ts
@@ -0,0 +1,33 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+
+export const app = express();
+require("dotenv").config();
+
+// Body Parser
+app.use(express.json({ limit: "50mb" }));
+// cookie-parser
+app.use(cookieParser());
+// cors Policy
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// Test Api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  // console.log(`Server is Running`);
+  res.status(200).json({
+    success: true,
+    message: "Api is Working",
+  });
+});
+
+// Unknown Route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 400;
+  next(err);
+});
